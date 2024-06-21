16c6fd9b2fd543ee02b7ea0b1cb1fc9c93e78ee1
axelfrancop <afrancopedroso@gmail.com>
2024-06-21 09:45:04 -0300

    Initial Commit

---
+++ CapyTimer/CapyTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapyTimerApp.swift
+//  CapyTimer
+//
+//  Created by Axel Pedroso on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct CapyTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
